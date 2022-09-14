b1641abc05406a3621e859cdfbee7d0c55ea00ac
Carolina Jimenez Moreno <CJimenezm0794@gmail.com>
2022-09-14 17:01:24 -0500

    add - Enterprises web page

--- src/main/java/com/project/budgetguardian/Controladores/FrontControlador.java
+++ src/main/java/com/project/budgetguardian/Controladores/FrontControlador.java
@@ -1,17 +1,30 @@
 package com.project.budgetguardian.Controladores;
 
+import antlr.collections.List;
+import com.project.budgetguardian.Entidades.Empresa;
+import com.project.budgetguardian.Servicios.EmpresaServicio;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontControlador {
+
+    EmpresaServicio servicio;
+
+    public FrontControlador(EmpresaServicio servicio){
+        this.servicio = servicio;
+    }
+
     @GetMapping("/")
     public String index(){
         return "index";
     }
 
     @GetMapping("/enterprises")
-    public String enterprises(){
+    public String enterprises(Model model){
+        List<Empresa> empresas = this.servicio.getEmpresas();
+        model.addAttribute("empresas", empresas);
         return "enterprises";
     }
 }
