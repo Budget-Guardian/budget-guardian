0f470cb2cb6102f05a2da359c572c43888c867c3
Carolina Jimenez Moreno <CJimenezm0794@gmail.com>
2022-09-14 21:13:36 -0500

    edit - Exclude security auto configuration

--- src/main/java/com/project/budgetguardian/BudgetguardianApplication.java
+++ src/main/java/com/project/budgetguardian/BudgetguardianApplication.java
@@ -2,8 +2,9 @@ package com.project.budgetguardian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication()
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BudgetguardianApplication {
     public static void main(String[] args) {
         SpringApplication.run(BudgetguardianApplication.class, args);
